4ac0f70af3097869fa0ec10f259f2d5b94573c35
Sam Jespersen <heyheysamj@gmail.com>
2019-10-28 17:27:52 -0700

    do stuff

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const writePackage = require('./package-json-writer');
 const eslintWriter = require('./eslint-writer');
 const babelWriter = require('./babel-writer');
@@ -5,11 +7,26 @@ const webpackWriter = require('./webpack-writer');
 const travisWriter = require('./travis-writer');
 const gitignoreWriter = require('./gitignore-writer');
 const srcIndexWriter = require('./src-index-writer');
+const dependenciesInstaller = require('./dependencies-installer');
+const devDependenciesInstaller = require('./dev-dependencies-installer');
 
-writePackage('./test/package.json');
-eslintWriter('./test/.eslintrc');
-babelWriter('./test/.babelrc');
-webpackWriter('./test/webpack.config.js');
-travisWriter('./test/.travis.yml');
-gitignoreWriter('./test/.gitignore');
-srcIndexWriter('./test/src/');
+const userPath = process.argv[2];
+
+if(userPath !== '.' && !fs.existsSync(userPath)) {
+  fs.mkdirSync(userPath);
+}
+Promise.all([
+  writePackage(path.join(userPath, 'package.json')),
+  eslintWriter(path.join(userPath, '.eslintrc')),
+  babelWriter(path.join(userPath, '.babelrc')),
+  webpackWriter(path.join(userPath, 'webpack.config.js')),
+  travisWriter(path.join(userPath, '.travis.yml')),
+  gitignoreWriter(path.join(userPath, '.gitignore')),
+  srcIndexWriter(path.join(userPath, 'src/'))
+])
+  .then(() => {
+    dependenciesInstaller(userPath);
+    devDependenciesInstaller(userPath);
+  });
+
+
